548165114c05b8a006803f174937690b33df595b
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2021-09-16 09:44:30 +0200

    Updated default parameters for sagemaker

--- lua/execute_exporter.lua
+++ lua/execute_exporter.lua
@@ -14,30 +14,29 @@ function main(json_str)
 	local parameters_map = json.decode(json_str)
 
 	if not parameters_map['problem_type'] then
-		parameters_map['problem_type'] = 'classification' -- default : 'classification'
+		parameters_map['problem_type'] = nil
 	end
 
 	if not parameters_map['max_runtime_for_automl_job_in_seconds'] then
-		parameters_map['max_runtime_for_automl_job_in_seconds'] = 1000 -- default : 1000 sec.
+		parameters_map['max_runtime_for_automl_job_in_seconds'] = nil
 	end
 
 	if not parameters_map['max_runtime_per_training_job_in_seconds'] then
-		parameters_map['max_runtime_per_training_job_in_seconds'] = 300 -- default : 300 sec.
+		parameters_map['max_runtime_per_training_job_in_seconds'] = nil
 	end
 
 	if not parameters_map['max_candidates'] then
-		parameters_map['max_candidates'] = 50 -- default : 50
+		parameters_map['max_candidates'] = nil
 	end
 
 	if not parameters_map['objective'] then
-		parameters_map['objective'] = 'mean squared error' -- default : 'mean squared error'
+		parameters_map['objective'] = nil
 	end
 
 	parameters_map['compression_type'] = 'gzip' -- default : 'gzip'
 
 
 	local aws_s3_handler = require("aws_s3_handler").init(pquery)
-	-- import('AWS_S3_HANDLER', 's3_handler')
 	aws_s3_handler.export_to_s3(
 			parameters_map['input_schema_name']..'.'..parameters_map['input_table_or_view_name'],
 			parameters_map['aws_credentials_connection_name'],
