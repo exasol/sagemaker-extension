a35183748dbc20ef15ede12c1d1d8aedded28de2
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2021-10-15 10:43:21 +0200

    Revert "Get schema name implicitly"
    This reverts commit 9dfe734eafc3413379c80cf4f1cc4251dee19cb1.

--- tests/test_create_statement_training.py
+++ tests/test_create_statement_training.py
@@ -16,15 +16,6 @@ def open_schema(conn):
         conn.execute(query.format(schema_name=SCHEMA_NAME))
 
 
-def get_schema_name(conn):
-    query_create_script = "CREATE OR REPLACE SCRIPT get_schema_name () " \
-                          "AS output(exa.meta.script_schema)"
-    conn.execute(query_create_script)
-
-    query_run_script = "EXECUTE SCRIPT get_schema_name () WITH OUTPUT"
-    return conn.execute(query_run_script).fetchval()
-
-
 @pytest.fixture(scope="session")
 def setup_database():
     conn = pyexasol.connect(
@@ -32,16 +23,12 @@ def setup_database():
         user=DB_CONNECTION_USER,
         password=DB_CONNECTION_PASS)
 
-    global schema_name
-
     open_schema(conn)
-    schema_name = get_schema_name(conn)
     return conn
 
 
 def get_created_scripts(conn):
-    exa_all_scripts_list = conn.export_to_list(
-        "Select SCRIPT_NAME FROM EXA_ALL_SCRIPTS")
+    exa_all_scripts_list = conn.export_to_list("Select SCRIPT_NAME FROM EXA_ALL_SCRIPTS")
     return sum(exa_all_scripts_list, [])
 
 
@@ -50,11 +37,8 @@ def test_export_table(setup_database):
 
     with open(SQL_CREATE_STATEMENT_FILE_PATH) as f:
         statement_str = f.read()
-    statement_str = statement_str.format(SCHEMA_NAME=schema_name)
     db_conn.execute(statement_str)
 
     exa_all_created_scripts = get_created_scripts(db_conn)
     assert CREATE_SCRIPT_NAME in exa_all_created_scripts
-
-
 
