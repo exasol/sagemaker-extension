64a37ffe5cd813e27ee6d20f72a47497a1e37751
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2021-10-18 08:14:32 +0200

    Updated deployment class variables

--- scripts/deploy_create_statements.py
+++ scripts/deploy_create_statements.py
@@ -18,37 +18,36 @@ class DeployCreateStatements:
         self._db_pass = kwargs['pass']
         self._schema = kwargs['schema']
         self._to_print = kwargs['print']
-        self.__exasol_conn = None
-        self.__statements = []
+        self.__exasol_conn = pyexasol.connect(
+            dsn="{host}:{port}".format(host=self._db_host, port=self._db_port),
+            user=self._db_user,
+            password=self._db_pass,
+            compression=True)
 
     def run(self):
-        self._create_exporting_statement()
-        self._create_training_statement()
+        statement_list = []
+        exportig_create_stmt = self._create_exporting_statement()
+        training_create_stmt = self._create_training_statement()
+        statement_list = [
+                exportig_create_stmt,
+                training_create_stmt
+            ]
         if not self._to_print:
-            self._connect_to_database()
             self._open_schema()
-            self._execute_statements()
+            self._execute_statements(statement_list)
         else:
-            print("\n".join(self.__statements))
+            print("\n".join(statement_list))
 
     def _create_exporting_statement(self):
         subprocess.call(EXPORTING_CREATE_STATEMENT_EXEC_PATH, shell=True)
         with open(EXPORTING_CREATE_STATEMENT_SQL_PATH) as f:
             statement_str = f.read()
-            self.__statements.append(statement_str)
+        return statement_str
 
     def _create_training_statement(self):
         with open(TRAINING_CREATE_STATEMENT_SQL_PATH) as f:
             statement_str = f.read()
-            self.__statements.append(statement_str)
-
-    def _connect_to_database(self):
-        db_connection_str = "{host}:{port}".format(
-            host=self._db_host, port=self._db_port)
-
-        self.__exasol_conn = pyexasol.connect(
-            dsn=db_connection_str, user=self._db_user,
-            password=self._db_pass, compression=True)
+        return statement_str
 
     def _open_schema(self):
         queries = ["CREATE SCHEMA IF NOT EXISTS {schema_name}",
@@ -56,8 +55,8 @@ class DeployCreateStatements:
         for query in queries:
             self.__exasol_conn.execute(query.format(schema_name=self._schema))
 
-    def _execute_statements(self):
-        for stmt in self.__statements:
+    def _execute_statements(self, statement_list: list):
+        for stmt in statement_list:
             self.__exasol_conn.execute(stmt)
 
 
