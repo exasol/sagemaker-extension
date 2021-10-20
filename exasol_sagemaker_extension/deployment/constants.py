2c56de51c042a60e02603366ec187a84bdc7699d
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2021-10-20 09:10:29 +0200

    Used context manager for tempfile

--- exasol_sagemaker_extension/deployment/constants.py
+++ exasol_sagemaker_extension/deployment/constants.py
@@ -1,6 +1,5 @@
 import os.path
 import logging
-import tempfile
 import importlib_resources
 
 # set logger
@@ -9,20 +8,13 @@ logging.basicConfig(
     level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# create tmp folder
-TMP_DIR_OBJ = tempfile.TemporaryDirectory()
-TMP_DIR = TMP_DIR_OBJ.name
-
 # set deployment paths
 BASE_DIR = importlib_resources.files("exasol_sagemaker_extension")
 LUA_SRC_DIR = (BASE_DIR / "lua" / "src")
 
 LUA_SRC_EXECUTER = "execute_exporter.lua"
 LUA_SRC_AWS_HANDLER = "aws_s3_handler.lua"
-
-LUA_BUNDLED_SOURCES_PATH = os.path.join(TMP_DIR, "bundle_sources.lua")
-LUA_BUNDLED_EXAERROR_PATH = os.path.join(TMP_DIR, "bundle_exaerror.lua")
-LUA_BUNDLED_FINAL_PATH = os.path.join(TMP_DIR, "bundle_final.lua")
+LUA_BUNDLED = "bundle_final.lua"
 
 EXPORTING_CREATE_STATEMENT_TEMPLATE_SQL_PATH_OBJ = BASE_DIR.joinpath(
     "resources").joinpath("create_statement_exporting_template.sql")
--- exasol_sagemaker_extension/deployment/generate_create_statement_exporting_sql.py
+++ exasol_sagemaker_extension/deployment/generate_create_statement_exporting_sql.py
@@ -1,5 +1,5 @@
-import shutil
 import os.path
+import tempfile
 import subprocess
 from typing import List
 from pathlib import Path
@@ -26,51 +26,48 @@ class BaseCreateStatementGenerator:
         self._lua_modules = modules
         self._lua_modules_str = " ".join(modules)
 
-    def _copy_lua_source_files(self):
+    def _copy_lua_source_files(self, tmp_dir: str):
         """
         Copy Lua source files into the temporary directory where amalg script
         is executed and the bundle script is generated
+
+        :param str tmp_dir: temporary directory  path
         """
         for lua_src_file in self._lua_copy_source_list:
             src_data = (constants.LUA_SRC_DIR / lua_src_file).read_text()
-            with open(os.path.join(
-                    constants.TMP_DIR, lua_src_file), "w") as file:
+            with open(os.path.join(tmp_dir, lua_src_file), "w") as file:
                 file.write(src_data)
-                logger.debug(f"Copy {lua_src_file} to {constants.TMP_DIR}")
+                logger.debug(f"Copy {lua_src_file} to {tmp_dir}")
 
-    def _bundle_lua_scripts(self):
+    def _bundle_lua_scripts(self, tmp_dir: str):
         """
         Executes amalg.lua script to bundle given Lua modules.
+
+        :param str tmp_dir: temporary directory  path
         """
+        lua_bundle_path = os.path.join(tmp_dir, constants.LUA_BUNDLED)
         bash_command = \
             "cd {tmp_dir} && amalg.lua -o {fin_path} -s {modules}".format(
-                tmp_dir=constants.TMP_DIR,
-                fin_path=constants.LUA_BUNDLED_FINAL_PATH,
+                tmp_dir=tmp_dir,
+                fin_path=lua_bundle_path,
                 modules=self._lua_modules_str)
 
         subprocess.check_call(bash_command, shell=True)
-        logger.debug(f"Lua scripts are bundled into "
-                     f"{constants.LUA_BUNDLED_FINAL_PATH}")
+        logger.debug(f"Lua scripts are bundled into {lua_bundle_path}")
 
-    def _remove_tmp_dir(self):
-        """
-        Remove the temporary directory when bundle operation is completed.
-        """
-        if os.path.exists(constants.TMP_DIR):
-            constants.TMP_DIR_OBJ.cleanup()
-            logger.debug("Temporary directory is removed")
-
-    def _insert_bundle_into_sql_script(self):
+    def _insert_bundle_into_sql_script(self, tmp_dir: str):
         """
         Insert the bundled Lua script read in  temporary directory to
         the CREATE SCRIPT sql statement read from Resources.
+
+        :param str tmp_dir: temporary directory  path
 
         :return str: completed CREATE SCRIPT sql statement
         """
         sql_tmplate = constants.\
             EXPORTING_CREATE_STATEMENT_TEMPLATE_SQL_PATH_OBJ.read_text()
 
-        with open(constants.LUA_BUNDLED_FINAL_PATH, "r") as file:
+        with open(os.path.join(tmp_dir, constants.LUA_BUNDLED), "r") as file:
             lua_bundled_data = file.read()
 
         logger.debug("Bundled script is inserted into create statement")
@@ -83,11 +80,11 @@ class BaseCreateStatementGenerator:
 
         :return str: completed CREATE SCRIPT sql statement
         """
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            self._copy_lua_source_files(tmp_dir)
+            self._bundle_lua_scripts(tmp_dir)
+            stmt = self._insert_bundle_into_sql_script(tmp_dir)
 
-        self._copy_lua_source_files()
-        self._bundle_lua_scripts()
-        stmt = self._insert_bundle_into_sql_script()
-        self._remove_tmp_dir()
         return stmt
 
 
