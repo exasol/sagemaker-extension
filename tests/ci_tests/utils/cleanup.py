4283471743cffd099565ec64a51cf88340aae840
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-01-26 13:20:55 +0100

    Remove the redundant methods

--- tests/ci_tests/utils/cleanup.py
+++ tests/ci_tests/utils/cleanup.py
@@ -1,19 +1,5 @@
-import boto3
 import pytest
 from tests.ci_tests.utils.autopilot_deletion import AutopilotTestDeletion
-
-
-def delete_endpoint(endpoint_name, model_setup_params, db_conn):
-    print("Delete the created endpoint")
-    AutopilotTestDeletion.delete_endpoint(
-        endpoint_name, model_setup_params, db_conn)
-
-
-def delete_s3_bucket():
-    print("Delete the created s3 bucket")
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('my-bucket')
-    bucket.delete()
 
 
 def cleanup(func):
@@ -27,8 +13,9 @@ def cleanup(func):
             print("Exception occurred while running the test: %s" % exc)
             raise pytest.fail(exc)
         finally:
-            delete_endpoint(endpoint_name, model_setup_params, db_conn)
-            delete_s3_bucket()
+            print("Delete the created endpoint")
+            AutopilotTestDeletion.delete_endpoint(
+                endpoint_name, model_setup_params, db_conn)
 
     return wrapper
 
