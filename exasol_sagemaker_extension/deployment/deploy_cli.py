9ab795fe44b705e84433b41be5d3a6b0eb654088
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-02-17 17:49:41 +0100

    Updated variable names

--- exasol_sagemaker_extension/deployment/deploy_cli.py
+++ exasol_sagemaker_extension/deployment/deploy_cli.py
@@ -8,14 +8,14 @@ from exasol_sagemaker_extension.deployment.deploy_create_statements \
 @click.option('--host', type=str, required=True, help="db host address")
 @click.option('--port', type=str, required=True, help="db host port")
 @click.option('--user', type=str, required=True, help="db user name")
-@click.option('--pass', 'pass_', required=True, help="db user password")
+@click.option('--pass', 'pwd', required=True, help="db user password")
 @click.option('--schema', type=str, required=True, help="schema name")
-@click.option('--print', 'print_', type=bool, required=False,
+@click.option('--print', 'verbose', type=bool, required=False,
               is_flag=True, help="print out statements")
 @click.option('--develop', type=bool, required=False,
               is_flag=True, help="generate and execute the scripts")
-def main(host: str, port: str, user: str, pass_: str, schema: str,
-         print_: bool = False, develop: bool = False):
+def main(host: str, port: str, user: str, pwd: str, schema: str,
+         verbose: bool = False, develop: bool = False):
 
     logging.basicConfig(format='%(asctime)s - %(module)s  - %(message)s',
                         level=logging.DEBUG)
@@ -24,9 +24,9 @@ def main(host: str, port: str, user: str, pass_: str, schema: str,
         db_host=host,
         db_port=port,
         db_user=user,
-        db_pass=pass_,
+        db_pass=pwd,
         schema=schema,
-        to_print=print_,
+        to_print=verbose,
         develop=develop
     )
     deployment.run()
