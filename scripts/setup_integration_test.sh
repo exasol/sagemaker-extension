2586fe43b13d48db1a1089fecd701646c15e7bfa
Torsten Kilias <torsten.kilias@exasol.com>
2023-08-15 11:06:34 +0200

    Fix pin ITDE to version 1.6.0 [run aws tests]

--- scripts/setup_integration_test.sh
+++ scripts/setup_integration_test.sh
@@ -20,8 +20,7 @@ function install_localstack {
 function checkout_exasol_test_container {
   cnt_func=$((cnt_func+1))
   echo -e "${YEL} Step-$cnt_func: ${GRA} Checkout Exasol test container ${NC}"
-   git clone https://github.com/exasol/integration-test-docker-environment.git
-   git switch 1.6.0
+   git clone https://github.com/exasol/integration-test-docker-environment.git 1.6.0
 }
 
 function spawn_exasol_environment {
