02d59de17ecec6c72cd980aa2785f39883cc735c
mibe <mikhail.beck@exasol.com>
2024-06-26 10:56:24 +0100

    #120 Moved pyexasol connect to the python-extension-common

--- exasol_sagemaker_extension/deployment/deploy_create_statements.py
+++ exasol_sagemaker_extension/deployment/deploy_create_statements.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 import logging
 
 import pyexasol
-from exasol.python_extension_common.deployment.language_container_deployer import get_websocket_sslopt
-from exasol.saas.client.api_access import get_connection_params
+from exasol.python_extension_common.connections.pyexasol_connection import open_pyexasol_connection
 
 from exasol_sagemaker_extension.deployment import constants
 from exasol_sagemaker_extension.deployment. \
@@ -124,67 +123,10 @@ class DeployCreateStatements:
     @classmethod
     def create_and_run(cls,
                        schema: str,
-                       dsn: str | None = None,
-                       db_user: str | None = None,
-                       db_pass: str | None = None,
-                       saas_url: str | None = None,
-                       saas_account_id: str | None = None,
-                       saas_database_id: str | None = None,
-                       saas_database_name: str | None = None,
-                       saas_token: str | None = None,
-                       use_ssl_cert_validation: bool = True,
-                       ssl_trusted_ca: str | None = None,
-                       ssl_client_certificate: str | None = None,
-                       ssl_private_key: str | None = None,
                        to_print: bool = False,
-                       develop: bool = False):
-        """
-        Creates a database connection object, either in an On-Prem or SaaS database,
-        based on the provided credentials. Creates an instance of the DeployCreateStatements
-        passing the connection object to it and calls its run method.
+                       develop: bool = False,
+                       **kwargs):
 
-        Parameters:
-            schema              - Schema where the scripts should be created
-            dsn                 - On-Prem database host address, including the port
-            db_user             - On-Prem database username
-            db_pass             - On-Prem database user password
-            saas_url            - SaaS service url
-            saas_account_id     - SaaS account id
-            saas_database_id    - SaaS database id
-            saas_database_name  - SaaS database name, to be used in case the id is unknown
-            saas_token          - SaaS Personal Access Token (PAT)
-            use_ssl_cert_validation - Use SSL server certificate validation
-            ssl_trusted_ca          - Path to a file or directory with a trusted CA bundle
-            ssl_client_certificate  - Path to a file with the client SSL certificate
-            ssl_private_key         - Path to a file with the client private key
-            to_print            - If True the script creation SQL commands will be
-                                  printed rather than executed
-            develop             - If True the scripts will be generated from scratch
-        """
-
-        # Infer where the database is - on-prem or SaaS.
-        if all((dsn, db_user, db_pass)):
-            connection_params = {'dsn': dsn, 'user': db_user, 'password': db_pass}
-        elif all((saas_url, saas_account_id, saas_token,
-                  any((saas_database_id, saas_database_name)))):
-            connection_params = get_connection_params(host=saas_url,
-                                                      account_id=saas_account_id,
-                                                      database_id=saas_database_id,
-                                                      database_name=saas_database_name,
-                                                      pat=saas_token)
-        else:
-            raise ValueError('Incomplete parameter list. '
-                             'Please either provide the parameters [dns, db_user, db_pass] '
-                             'for an On-Prem database or [saas_url, saas_account_id, '
-                             'saas_database_id, saas_token] for a SaaS database.')
-
-        websocket_sslopt = get_websocket_sslopt(use_ssl_cert_validation, ssl_trusted_ca,
-                                                ssl_client_certificate, ssl_private_key)
-
-        exasol_conn = pyexasol.connect(**connection_params,
-                                       encryption=True,
-                                       websocket_sslopt=websocket_sslopt,
-                                       compression=True)
-
+        exasol_conn = open_pyexasol_connection(schema=schema, **kwargs)
         deployer = cls(exasol_conn, schema, to_print, develop)
         deployer.run()
