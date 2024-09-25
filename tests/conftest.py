2a7a72c7d5c6c9130ab42cfaf010dc90432f66c4
mibe <mikhail.beck@exasol.com>
2024-09-25 13:03:41 +0100

    #140 Fixed the contest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 pytest_plugins = [
     "tests.fixtures.aws_connection_fixture",
-    "tests.fixtures.database_connection_fixture",
+    "tests.fixtures.language_container_fixture",
     "tests.fixtures.setup_database_fixture",
     "tests.fixtures.prepare_environment_fixture",
     "tests.fixtures.script_deployment_fixture",
