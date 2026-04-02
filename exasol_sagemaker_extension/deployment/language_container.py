8fa8ff7cca9f64604c12eee03a0dc32916c424b9
mibe <mikhail.beck@exasol.com>
2026-04-02 10:59:54 +0100

    Added CPU Torch

--- exasol_sagemaker_extension/deployment/language_container.py
+++ exasol_sagemaker_extension/deployment/language_container.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 from contextlib import contextmanager
 
 from exasol.python_extension_common.deployment.language_container_builder import (
@@ -8,9 +9,25 @@ from exasol.python_extension_common.deployment.language_container_builder import
 CONTAINER_NAME = "exasol_sagemaker_extension_container"
 
 
+def add_pytorch_to_requirements(container_builder: LanguageContainerBuilder) -> None:
+    """Modifies the default dependencies/Dockerfile"""
+    dockerfile_file = "flavor_base/dependencies/Dockerfile"
+    dockerfile = container_builder.read_file(dockerfile_file)
+    install_pattern = (
+        r"^\s*(?i:run)\s+python\d.\d+\s+-m\s+pip\s+install"
+        r"\s+-r\s+/project/requirements.txt"
+    )
+    install_extra = "--extra-index-url https://download.pytorch.org/whl/cpu"
+    dockerfile = re.sub(
+        install_pattern, rf"\g<0> {install_extra}", dockerfile, flags=re.MULTILINE
+    )
+    container_builder.write_file(dockerfile_file, dockerfile)
+
+
 @contextmanager
 def language_container_factory():
     with LanguageContainerBuilder(CONTAINER_NAME) as container_builder:
+        add_pytorch_to_requirements(container_builder)
         project_directory = find_path_backwards("pyproject.toml", __file__).parent
         container_builder.prepare_flavor(project_directory, requirement_filter=exclude_cuda)
         yield container_builder
